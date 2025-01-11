7cecfb38c9acd5d9d258fa7b1dec37d7da2ab2a7
TikamporhnB <bonyawat2004@gmail.com>
2025-01-11 20:10:17 +0700

    Update web.php

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 
-// แสดงฟอร์ม
 Route::get('/mycontroller', [MyController::class, 'showForm']);
 
-// แสดงตารางสูตรคูณ
 Route::post('/mycontroller', [MyController::class, 'showTable']);
 
 
