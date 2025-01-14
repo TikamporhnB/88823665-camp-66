b8e21e5c45395d11fd020e58f22b6500c7467dc5
TikamporhnB <bonyawat2004@gmail.com>
2025-01-14 19:55:21 +0700

    laravel

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
+Route::get('/register' ,
+[RegisterController::class, 'index']);
+
+Route::get('/login' ,
+[LoginController::class,'index']);
 
 Route::get('/mycontroller', 
 [MyController::class, 'showForm']);
